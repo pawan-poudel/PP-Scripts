3f20087bab613ed01a1847437b7898fa8c53422e
pawan-poudel <ppoudel@icr.ac.uk>
2016-04-18 15:25:27 +0100

    PreprocessMOT updated

--- PP-Scripts/preprocessMOT/R/impute_missing_values.R
+++ PP-Scripts/preprocessMOT/R/impute_missing_values.R
@@ -1,17 +1,11 @@
 impute_missing_values <-
 function(pancan12_genomeMatrix){
   
-  
-  rownames(pancan12_genomeMatrix)=pancan12_genomeMatrix[,1]
-  
   pancan12_genomeMatrix_1=apply(pancan12_genomeMatrix[,-1], 2, as.numeric)
-  rownames(pancan12_genomeMatrix_1)=pancan12_genomeMatrix[,1]
+  genes=pancan12_genomeMatrix[,1]
   pancan12_genomeMatrix.imputed=impute.knn(as.matrix(pancan12_genomeMatrix_1))
-  #pancan12_genomeMatrix.imputed
-  genes=rownames(pancan12_genomeMatrix.imputed$data)
   rownames(pancan12_genomeMatrix.imputed$data)=NULL
   data_to_write=cbind(genes,pancan12_genomeMatrix.imputed$data)
-  rownames(data_to_write)=NULL
   colnames(data_to_write)[1]="Genes"
 
   #write.table(data_to_write, file, sep='\t')
